85b99b7ac36adc2de4b121006882a99b754807cf
Michiel Fortuin <michiel.fortuin@gmail.com>
2015-03-03 12:05:09 +0100

    simplified the new command

--- operations/MenuFileOperations.py
+++ operations/MenuFileOperations.py
@@ -4,28 +4,6 @@ import operations.BaseOperation
 import helper
 import json
 
-
-'''
-class New(open):
-    #emptyDocumentFilename = "emptyFile.jps"
-    emptyDocumentFilename = "testFile.jps"
-
-    def __init__(self):
-        super(New, self).__init__(New.emptyDocumentFilename)
-        print("WARNING: loading test file in place of the standard empty file")
-'''
-
-class New(operations.BaseOperation.operation):
-    def __init__(self):
-        self.filename = "testFile.jps"
-
-    def run(self, model):
-        f = open(self.filename, 'r')
-        model.SceneDesc = json.load(f)
-        f.close()
-        helper.CallObservers(model.SceneDescChanged)
-
-
 class Open(operations.BaseOperation.operation):
     def __init__(self, filename):
         self.filename = filename
@@ -45,3 +23,11 @@ class Save(operations.BaseOperation.operation):
         f = open(self.filename, 'w')
         json.dump(model.SceneDesc, f)
         f.close()
+
+class New(Open):
+    #emptyDocumentFilename = "emptyFile.jps"
+    emptyDocumentFilename = "testFile.jps"
+
+    def __init__(self):
+        super(New, self).__init__(New.emptyDocumentFilename)
+        print("WARNING: loading test file in place of the standard empty file")
