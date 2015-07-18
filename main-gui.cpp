a3eac4c91ea4b784dee74f87f06bac04be37fd0c
Michiel Fortuin <michiel.fortuin@gmail.com>
2015-07-18 11:38:44 +0200

    Added a second target OpenPSTD-GUI and renamed OpenPSTD to OpenPSTD-CLI Added basic hello world example to main-gui.cpp renamed main.cpp to main-cli.cpp

---
+++ main-gui.cpp
@@ -0,0 +1,11 @@
+//
+// Created by michiel on 18-7-2015.
+//
+
+#include <iostream>
+#include "main-gui.h"
+
+int main(int argc, const char *argv[])
+{
+    std::cout << "Hello world!" << std::endl;
+}
---
+++ main-gui.h
@@ -0,0 +1,8 @@
+//
+// Created by michiel on 18-7-2015.
+//
+
+#ifndef OPENPSTD_MAIN_GUI_H
+#define OPENPSTD_MAIN_GUI_H
+
+#endif //OPENPSTD_MAIN_GUI_H
