500543ae7eed2ac80de7648858f6371b9c3ca569
Michiel Fortuin <michiel.fortuin@gmail.com>
2015-07-31 12:28:39 +0200

    Implemented the Viewer2D widget so that it draws a triangle Added OpenGL to CMakeLists.txt

--- Viewer2D.cpp
+++ Viewer2D.cpp
@@ -3,3 +3,47 @@
 //
 
 #include "Viewer2D.h"
+
+Viewer2D::Viewer2D(QWidget *parent)
+    : QGLWidget(QGLFormat(QGL::SampleBuffers), parent)
+{
+
+}
+
+void Viewer2D::paintGL()
+{
+    glLoadIdentity();
+    glBegin(GL_TRIANGLES);
+    qglColor(Qt::red);
+    glVertex3f(-1,-1,0);
+    qglColor(Qt::blue);
+    glVertex3f(-1,1,0);
+    qglColor(Qt::yellow);
+    glVertex3f(1,1,0);
+    glEnd();
+}
+
+void Viewer2D::initializeGL()
+{
+    glClearColor(0, 0, 0, 1);
+
+    glDisable(GL_CULL_FACE);
+    glShadeModel(GL_SMOOTH);
+    glDisable(GL_LIGHTING);
+    glDisable(GL_LIGHT0);
+}
+
+void Viewer2D::resizeGL(int w, int h)
+{
+    glViewport(0, 0, h, w);
+}
+
+QSize Viewer2D::minimumSizeHint() const
+{
+    return QSize(50, 50);
+}
+
+QSize Viewer2D::sizeHint() const
+{
+    return QSize(400, 400);
+}
--- Viewer2D.h
+++ Viewer2D.h
@@ -5,10 +5,23 @@
 #ifndef OPENPSTD_VIEWER2D_H
 #define OPENPSTD_VIEWER2D_H
 
+#include <QGLWidget>
 
-class Viewer2D
+class Viewer2D: public QGLWidget
 {
+    Q_OBJECT
 
+public:
+    explicit Viewer2D(QWidget *parent = 0);
+
+public:
+    virtual QSize sizeHint() const override;
+    virtual QSize minimumSizeHint() const override;
+
+protected:
+    virtual void resizeGL(int w, int h) override;
+    virtual void initializeGL() override;
+    virtual void paintGL() override;
 };
 
 
