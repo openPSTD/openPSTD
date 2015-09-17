cdbb9402758a6463a2656e9654cc09ca55007019
Louisvh <Louis.van.harten@gmail.com>
2015-09-17 11:51:44 +0200

    added openPSTD headers to all .{cpp,h} files, and a template for Date/Authors/Purpose

--- PSTDFile.h
+++ PSTDFile.h
@@ -1,6 +1,34 @@
+//////////////////////////////////////////////////////////////////////////
+// This file is part of openPSTD.                                       //
+//                                                                      //
+// openPSTD is free software: you can redistribute it and/or modify     //
+// it under the terms of the GNU General Public License as published by //
+// the Free Software Foundation, either version 3 of the License, or    //
+// (at your option) any later version.                                  //
+//                                                                      //
+// openPSTD is distributed in the hope that it will be useful,          //
+// but WITHOUT ANY WARRANTY; without even the implied warranty of       //
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the        //
+// GNU General Public License for more details.                         //
+//                                                                      //
+// You should have received a copy of the GNU General Public License    //
+// along with openPSTD.  If not, see <http://www.gnu.org/licenses/\>.    //
+//                                                                      //
+//////////////////////////////////////////////////////////////////////////
+
+//////////////////////////////////////////////////////////////////////////
 //
-// Created by michiel on 18-7-2015.
+// Date:
+//      18-7-2015
 //
+// Authors:
+//      Michiel Fortuin
+//
+// Purpose:
+//      PSTDFile contains the configurations that the kernel will use
+//      to run the simulation.
+//
+//////////////////////////////////////////////////////////////////////////
 
 #ifndef OPENPSTD_PSTDFILE_H
 #define OPENPSTD_PSTDFILE_H
--- kernel/core/kernel_functions.h
+++ kernel/core/kernel_functions.h
@@ -1,6 +1,31 @@
+//////////////////////////////////////////////////////////////////////////
+// This file is part of openPSTD.                                       //
+//                                                                      //
+// openPSTD is free software: you can redistribute it and/or modify     //
+// it under the terms of the GNU General Public License as published by //
+// the Free Software Foundation, either version 3 of the License, or    //
+// (at your option) any later version.                                  //
+//                                                                      //
+// openPSTD is distributed in the hope that it will be useful,          //
+// but WITHOUT ANY WARRANTY; without even the implied warranty of       //
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the        //
+// GNU General Public License for more details.                         //
+//                                                                      //
+// You should have received a copy of the GNU General Public License    //
+// along with openPSTD.  If not, see <http://www.gnu.org/licenses/>.    //
+//                                                                      //
+//////////////////////////////////////////////////////////////////////////
+
+//////////////////////////////////////////////////////////////////////////
 //
-// Created by omar on 3-9-15.
+// Authors:
+//      Omar Richardson
+//      Louis van Harten
+// Purpose:
+//      Contains various functions that don't strictly belong to a class.
 //
+//////////////////////////////////////////////////////////////////////////
+
 
 #ifndef OPENPSTD_KERNEL_FUNCTIONS_H
 #define OPENPSTD_KERNEL_FUNCTIONS_H
