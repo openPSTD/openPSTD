a8f0f4ad8ef3a3aa964f4173b6fe12cc07bf3f07
Michiel Fortuin <michiel.fortuin@gmail.com>
2015-10-31 16:15:34 +0100

    updated trusty

--- build/install_Qt55.sh
+++ build/install_Qt55.sh
@@ -1,2 +1,3 @@
-sudo apt-get install qt553d qt55canvas3d
-sudo apt-get install qtcreator-latest
+#sudo apt-get install qt553d qt55canvas3d
+#sudo apt-get install qtcreator-latest
+sudo apt-get install qt5-default
