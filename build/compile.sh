45633caaccab256ee04080518b26edc835271a7a
Michiel Fortuin <michiel.fortuin@gmail.com>
2015-10-31 15:58:52 +0100

    changed compile.sh

--- build/compile.sh
+++ build/compile.sh
@@ -1,8 +1,9 @@
-CMAKE_OPTIONS=\-D RAPIDJSON_ROOT:PATH=/home/travis/build/micfort/openPSTD/rapidjson
-CMAKE_OPTIONS=${CMAKE_OPTIONS} \-D UNQLITE_INCLUDE:PATH=/home/travis/build/micfort/openPSTD/unqlite
-CMAKE_OPTIONS=${CMAKE_OPTIONS} \-D UNQLITE_LIB:PATH=/home/travis/build/micfort/openPSTD/unqlite/libunqlite.a
-#CMAKE_OPTIONS=${CMAKE_OPTIONS} -D EIGEN_INCLUDE:PATH=/home/travis/build/micfort/openPSTD/rapidjson
-CMAKE_OPTIONS=${CMAKE_OPTIONS} \-D Qt5_DIR:PATH=/opt/qt55
+#-D EIGEN_INCLUDE:PATH=/home/travis/build/micfort/openPSTD/rapidjson
 
-cmake ${CMAKE_OPTIONS} -G Unix\ Makefiles --debug-output
+cmake \
+	-D RAPIDJSON_ROOT:PATH=/home/travis/build/micfort/openPSTD/rapidjson \
+	-D UNQLITE_INCLUDE:PATH=/home/travis/build/micfort/openPSTD/unqlite \
+	-D UNQLITE_LIB:PATH=/home/travis/build/micfort/openPSTD/unqlite/libunqlite.a \
+	-D Qt5_DIR:PATH=/opt/qt55 \
+	-G Unix\ Makefiles --debug-output
 make OpenPSTD-gui VERBOSE=1
