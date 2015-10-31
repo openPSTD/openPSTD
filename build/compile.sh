286e741cc6ac4bac63795c011344cc1e2f8e8b7d
Michiel Fortuin <michiel.fortuin@gmail.com>
2015-10-31 16:37:04 +0100

    update

--- build/compile.sh
+++ build/compile.sh
@@ -1,9 +1,9 @@
 #-D EIGEN_INCLUDE:PATH=/home/travis/build/micfort/openPSTD/rapidjson
+#-D Qt5_DIR:PATH=/opt/qt55 \
 
 cmake \
 	-D RAPIDJSON_ROOT:PATH=/home/travis/build/micfort/openPSTD/rapidjson \
 	-D UNQLITE_INCLUDE:PATH=/home/travis/build/micfort/openPSTD/unqlite \
 	-D UNQLITE_LIB:PATH=/home/travis/build/micfort/openPSTD/unqlite/libunqlite.a \
-	-D Qt5_DIR:PATH=/opt/qt55 \
-	-G Unix\ Makefiles --debug-output
+	-G Unix\ Makefiles ./
 make OpenPSTD-gui VERBOSE=1
