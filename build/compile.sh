05936173b1bdb650245e0a0fe7293fd2221d5779
Michiel Fortuin <michiel.fortuin@gmail.com>
2015-10-31 15:35:40 +0100

    added compile.sh added configuration in the command line of cmake removed CMakeCache.txt more debug data from cmake

---
+++ build/compile.sh
@@ -0,0 +1,8 @@
+CMAKE_OPTIONS=-D RAPIDJSON_ROOT:PATH=/home/travis/build/micfort/openPSTD/rapidjson
+CMAKE_OPTIONS=${CMAKE_OPTIONS} -D UNQLITE_INCLUDE:PATH=/home/travis/build/micfort/openPSTD/unqlite
+CMAKE_OPTIONS=${CMAKE_OPTIONS} -D UNQLITE_LIB:PATH=/home/travis/build/micfort/openPSTD/unqlite/libunqlite.a
+#CMAKE_OPTIONS=${CMAKE_OPTIONS} -D EIGEN_INCLUDE:PATH=/home/travis/build/micfort/openPSTD/rapidjson
+CMAKE_OPTIONS=${CMAKE_OPTIONS} -D Qt5_DIR:PATH=/opt/qt55
+
+cmake ${CMAKE_OPTIONS} -G Unix\ Makefiles --debug-output
+make OpenPSTD-gui VERBOSE=1
