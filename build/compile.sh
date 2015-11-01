a79c5831034a5fb3b82f84e0b48efbe9eb1ce472
Michiel Fortuin <michiel.fortuin@gmail.com>
2015-11-01 10:44:20 +0100

    installs Qt5.5

--- build/compile.sh
+++ build/compile.sh
@@ -5,5 +5,6 @@ cmake \
 	-D RAPIDJSON_ROOT:PATH=/home/travis/build/micfort/openPSTD/rapidjson \
 	-D UNQLITE_INCLUDE:PATH=/home/travis/build/micfort/openPSTD/unqlite \
 	-D UNQLITE_LIB:PATH=/home/travis/build/micfort/openPSTD/unqlite/libunqlite.a \
+	-D Qt5_DIR:PATH=/opt/qt55 \
 	-G Unix\ Makefiles ./
 make OpenPSTD-gui VERBOSE=1
--- build/install_Qt55.sh
+++ build/install_Qt55.sh
@@ -1,3 +1,3 @@
-#sudo apt-get install qt553d qt55canvas3d
-#sudo apt-get install qtcreator-latest
-sudo apt-get -y install qt5-default
+sudo apt-get -y install qt553d qt55canvas3d
+sudo apt-get -y install qtcreator-latest
+#sudo apt-get -y install qt5-default
