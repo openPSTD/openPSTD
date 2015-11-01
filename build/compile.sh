adcba6d114fab3184a0ec55d0f2efb975104b539
Michiel Fortuin <michiel.fortuin@gmail.com>
2015-11-01 15:54:03 +0100

    Moved to relative paths Removed debug information

--- build/compile.sh
+++ build/compile.sh
@@ -2,9 +2,9 @@
 #-D Qt5_DIR:PATH=/opt/qt55 \
 
 cmake \
-	-D RAPIDJSON_ROOT:PATH=/home/travis/build/micfort/openPSTD/rapidjson \
-	-D UNQLITE_INCLUDE:PATH=/home/travis/build/micfort/openPSTD/unqlite \
-	-D UNQLITE_LIB:PATH=/home/travis/build/micfort/openPSTD/unqlite/libunqlite.a \
+	-D RAPIDJSON_ROOT:PATH=rapidjson \
+	-D UNQLITE_INCLUDE:PATH=unqlite \
+	-D UNQLITE_LIB:PATH=unqlite/libunqlite.a \
 	-D Qt5_DIR:PATH=/opt/qt55 \
 	-G Unix\ Makefiles ./
 make OpenPSTD-gui-test VERBOSE=1
