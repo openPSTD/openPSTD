f1305a09b964aeba4126cf599eb6ec01c5d5b57e
Michiel Fortuin <michiel.fortuin@gmail.com>
2015-11-01 15:52:45 +0100

    Added test of Edges

--- Edges-test.cpp
+++ Edges-test.cpp
@@ -39,28 +39,47 @@
 
 BOOST_AUTO_TEST_SUITE(GUI_Edges)
 
-    BOOST_AUTO_TEST_CASE(TestVertical)
-    {
-        Edge e(QVector2D(0, 10), QVector2D(10, 10), 0, false);
-        BOOST_REQUIRE(e.IsHorizontal());
-    }
-
-    BOOST_AUTO_TEST_CASE(TestVertical2)
-    {
-        Edge e(QVector2D(10, 0), QVector2D(10, 10), 0, false);
-        BOOST_REQUIRE(e.IsHorizontal());
-    }
-
     BOOST_AUTO_TEST_CASE(TestHorizontal)
     {
-        Edge e(QVector2D(10, 0), QVector2D(10, 10), 0, false);
-        BOOST_REQUIRE(e.IsVertical());
+        BOOST_CHECK(Edge(QVector2D(0, 10), QVector2D(10, 10), 0, false).IsHorizontal());
+        BOOST_CHECK(!Edge(QVector2D(10, 0), QVector2D(10, 10), 0, false).IsHorizontal());
     }
 
-    BOOST_AUTO_TEST_CASE(TestHorizontal2)
+    BOOST_AUTO_TEST_CASE(TestVertical)
     {
-        Edge e(QVector2D(0, 10), QVector2D(10, 10), 0, false);
-        BOOST_REQUIRE(!e.IsVertical());
+        BOOST_CHECK(Edge(QVector2D(10, 0), QVector2D(10, 10), 0, false).IsVertical());
+        BOOST_CHECK(!Edge(QVector2D(0, 10), QVector2D(10, 10), 0, false).IsVertical());
     }
+
+    BOOST_AUTO_TEST_CASE(TestGetStart)
+    {
+        BOOST_CHECK(Edge(QVector2D(0, 10), QVector2D(10, 10), 0, false).GetStart() == QVector2D(0, 10));
+    }
+
+    BOOST_AUTO_TEST_CASE(TestGetEnd)
+    {
+        BOOST_CHECK(Edge(QVector2D(0, 10), QVector2D(10, 10), 0, false).GetEnd() == QVector2D(10, 10));
+    }
+
+    BOOST_AUTO_TEST_CASE(TestOnSameLine)
+    {
+        BOOST_CHECK(Edge::OnSameLine(Edge(QVector2D(0, 10), QVector2D(10, 10), 0, false), Edge(QVector2D(0, 10), QVector2D(10, 10), 0, false)));
+        BOOST_CHECK(!Edge::OnSameLine(Edge(QVector2D(0, 11), QVector2D(10, 11), 0, false), Edge(QVector2D(0, 10), QVector2D(10, 10), 0, false)));
+        BOOST_CHECK(!Edge::OnSameLine(Edge(QVector2D(10, 0), QVector2D(10, 10), 0, false), Edge(QVector2D(0, 10), QVector2D(10, 10), 0, false)));
+        BOOST_CHECK(Edge::OnSameLine(Edge(QVector2D(0, 10), QVector2D(10, 10), 0, false), Edge(QVector2D(10, 10), QVector2D(20, 10), 0, false)));
+        BOOST_CHECK(!Edge::OnSameLine(Edge(QVector2D(0, 0), QVector2D(10, 10), 0, false), Edge(QVector2D(10, 10), QVector2D(20, 10), 0, false)));
+    }
+
+    BOOST_AUTO_TEST_CASE(TestSubstract)
+    {
+        BOOST_CHECK(Edge(QVector2D(0, 10), QVector2D(10, 10), 0, false).Substract(Edge(QVector2D(0, 10), QVector2D(10, 10), 0, false)).empty());
+
+        BOOST_CHECK(Edge(QVector2D(0, 10), QVector2D(10, 10), 0, false).Substract(Edge(QVector2D(0, 10), QVector2D(5, 10), 0, false))[0].GetStart() == QVector2D(5, 10));
+        BOOST_CHECK(Edge(QVector2D(0, 10), QVector2D(10, 10), 0, false).Substract(Edge(QVector2D(0, 10), QVector2D(5, 10), 0, false))[0].GetEnd() == QVector2D(10, 10));
+
+        BOOST_CHECK(Edge(QVector2D(0, 10), QVector2D(10, 10), 0, false).Substract(Edge(QVector2D(5, 10), QVector2D(10, 10), 0, false))[0].GetStart() == QVector2D(0, 10));
+        BOOST_CHECK(Edge(QVector2D(0, 10), QVector2D(10, 10), 0, false).Substract(Edge(QVector2D(5, 10), QVector2D(10, 10), 0, false))[0].GetEnd() == QVector2D(5, 10));
+    }
+
 
 BOOST_AUTO_TEST_SUITE_END()
