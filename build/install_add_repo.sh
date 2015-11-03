ee15c4d3e67358f750c10bdc506ea332a83814d6
Michiel Fortuin <michiel.fortuin@gmail.com>
2015-11-03 15:03:53 +0100

    brew update now writes everything to /dev/null

--- build/install_add_repo.sh
+++ build/install_add_repo.sh
@@ -3,6 +3,6 @@ if [ $TRAVIS_OS_NAME = "linux" ]; then
     sudo add-apt-repository --yes ppa:beineri/opt-qt55-trusty
     sudo apt-get update -qq
 else
-    sudo brew update
+    sudo brew update >/dev/null
 fi
 
