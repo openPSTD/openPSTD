ad9c0ec56b2abb6cfb780d9261154793fae33276
Michiel Fortuin <michiel.fortuin@gmail.com>
2015-11-10 10:56:33 +0100

    removed boost install step for mac os x

--- build/install_boost.sh
+++ build/install_boost.sh
@@ -3,13 +3,12 @@ set -e
 
 if [ ${TRAVIS_OS_NAME} = "linux" ]; then
     TOOLSET=gcc
+    wget http://sourceforge.net/projects/boost/files/boost/1.59.0/boost_1_59_0.tar.bz2/download -O boost_1_59_0.tar.bz2
+    tar -xjf boost_1_59_0.tar.bz2
+    cd boost_1_59_0/
+    ./bootstrap.sh --with-libraries=program_options,test
+    sudo ./b2 -d0 link=shared toolset=${TOOLSET} install
+    cd ../
 else
     TOOLSET=darwin
 fi
-
-wget http://sourceforge.net/projects/boost/files/boost/1.59.0/boost_1_59_0.tar.bz2/download -O boost_1_59_0.tar.bz2
-tar -xjf boost_1_59_0.tar.bz2
-cd boost_1_59_0/
-./bootstrap.sh --with-libraries=program_options,test
-sudo ./b2 -d0 link=shared toolset=${TOOLSET} install
-cd ../
