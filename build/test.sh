b6e8bdf4d7572d54e39552167abfbd427c3831e0
Michiel Fortuin <michiel.fortuin@gmail.com>
2015-11-10 11:22:16 +0100

    Add extra information in the test results (dependencies) removed debug information All targets are compiled against all libraries (boost)

--- build/test.sh
+++ build/test.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 set -e
 
+ldd OpenPSTD-gui
+ldd OpenPSTD-test
+
 ./OpenPSTD-test --log_format=XML --log_sink=results.xml --log_level=all --report_level=no
 xsltproc build/report/TestReport.xsl results.xml
