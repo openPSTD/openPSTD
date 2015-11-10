01d7fffceb45dce2fbc5f1e81fa69af717b028f9
Michiel Fortuin <michiel.fortuin@gmail.com>
2015-11-10 13:16:42 +0100

    Only checks dependencies

--- build/test.sh
+++ build/test.sh
@@ -3,15 +3,18 @@ set -e
 
 if [ ${TRAVIS_OS_NAME} = "linux" ]; then
     DEPENDENCY_CHECKER=ldd
+
+    echo Dependencies for OpenPSTD-gui
+    ${DEPENDENCY_CHECKER} OpenPSTD-gui
+
+    echo Dependencies for OpenPSTD-test
+    ${DEPENDENCY_CHECKER} OpenPSTD-test
 else
     DEPENDENCY_CHECKER=otool -L
+    #this should also work, but otool doesn't recognize -L, while it should
 fi
 
-echo Dependencies for OpenPSTD-gui
-${DEPENDENCY_CHECKER} OpenPSTD-gui
 
-echo Dependencies for OpenPSTD-test
-${DEPENDENCY_CHECKER} OpenPSTD-test
 
 ./OpenPSTD-test --log_format=XML --log_sink=results.xml --log_level=all --report_level=no
 xsltproc build/report/TestReport.xsl results.xml
