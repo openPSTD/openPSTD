62f843b2e40517d49114de5f4c2ac0df19028240
Michiel Fortuin <michiel.fortuin@gmail.com>
2015-11-10 11:34:00 +0100

    using otool on mac os x in place of ldd, als improved output

--- build/test.sh
+++ build/test.sh
@@ -1,8 +1,17 @@
 #!/usr/bin/env bash
 set -e
 
-ldd OpenPSTD-gui
-ldd OpenPSTD-test
+if [ ${TRAVIS_OS_NAME} = "linux" ]; then
+    DEPENDENCY_CHECKER=otool -L
+else
+    DEPENDENCY_CHECKER=ldd
+fi
+
+echo Dependencies for OpenPSTD-gui
+${DEPENDENCY_CHECKER} OpenPSTD-gui
+
+echo Dependencies for OpenPSTD-test
+${DEPENDENCY_CHECKER} OpenPSTD-test
 
 ./OpenPSTD-test --log_format=XML --log_sink=results.xml --log_level=all --report_level=no
 xsltproc build/report/TestReport.xsl results.xml
