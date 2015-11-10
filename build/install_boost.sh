85d16932ebc101b938ee112572006bf97d87036a
Michiel Fortuin <michiel.fortuin@gmail.com>
2015-11-10 10:32:09 +0100

    specify toolset in de compile boost stage

--- build/install_boost.sh
+++ build/install_boost.sh
@@ -1,9 +1,15 @@
 #!/usr/bin/env bash
 set -e
 
+if [ ${TRAVIS_OS_NAME} = "linux" ]; then
+    TOOLSET=gcc
+else
+    TOOLSET=darwin
+fi
+
 wget http://sourceforge.net/projects/boost/files/boost/1.59.0/boost_1_59_0.tar.bz2/download -O boost_1_59_0.tar.bz2
 tar -xjf boost_1_59_0.tar.bz2
 cd boost_1_59_0/
 ./bootstrap.sh --with-libraries=program_options,test
-sudo ./b2 -d0 link=shared install
+sudo ./b2 -d0 link=shared toolset=${TOOLSET} install
 cd ../
