8635c66a378c60cd0b0aed5e51f663a8a97b64cc
Michiel Fortuin <michiel.fortuin@gmail.com>
2015-12-17 17:00:20 +0100

    Made de kernel a separate target Removed Unqlite as external target and added it as an internal target

--- build/compile.sh
+++ build/compile.sh
@@ -11,8 +11,6 @@ fi
 
 cmake \
 	-D RAPIDJSON_ROOT:PATH=$PWD/rapidjson \
-	-D UNQLITE_INCLUDE:PATH=$PWD/unqlite \
-	-D UNQLITE_LIB:PATH=$PWD/unqlite/libunqlite.a \
 	-D EIGEN_INCLUDE:PATH=$PWD/eigen \
 	-D Qt5_DIR:PATH=${QT5DIR} \
 	-D CPACK_GENERATOR=${PACKAGE} \
@@ -24,6 +22,5 @@ cmake \
 
 make OpenPSTD-gui
 make OpenPSTD-test
-#make package
 sudo cpack -V -G ${PACKAGE}
 ls -alh
