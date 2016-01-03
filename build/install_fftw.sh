063ece155a84d6624866a0395b1cc5f6f8219a24
Louisvh <Louis.van.harten@gmail.com>
2016-01-03 16:41:49 +0100

    Travis needs positive reassurance
    Travis doesn't pipe "yes" into its input, so apt needs --yes

--- build/install_fftw.sh
+++ build/install_fftw.sh
@@ -5,7 +5,7 @@ if [ ${TRAVIS_OS_NAME} = "linux" ]; then
     echo running on linux
     echo installing fftw
     sudo apt-get update
-    sudo apt-get install libfftw3-dev
+    sudo apt-get  --yes --force-yes install libfftw3-dev
 else
     echo running on mac os x
     echo installing fftw with homebrew
