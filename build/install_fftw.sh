1da6a28cd43ef3c9f0d0375f48b1258a0b9b0f7e
Louisvh <Louis.van.harten@gmail.com>
2016-01-03 16:34:53 +0100

    Added fftw install to Travis

---
+++ build/install_fftw.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+set -e
+
+if [ ${TRAVIS_OS_NAME} = "linux" ]; then
+    echo running on linux
+    echo installing fftw
+    sudo apt-get update
+    sudo apt-get install libfftw3-dev
+else
+    echo running on mac os x
+    echo installing fftw with homebrew
+    sudo brew install fftw
+fi
