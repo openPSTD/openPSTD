277a2c1139eb6618d20e1872a441d8cacc635478
Louisvh <Louis.van.harten@gmail.com>
2016-01-04 18:09:51 +0100

    Also install the double version of fftw

--- build/install_fftw.sh
+++ build/install_fftw.sh
@@ -8,6 +8,8 @@ if [ ${TRAVIS_OS_NAME} = "linux" ]; then
     sudo apt-get  --yes --force-yes install libfftw3-dev
 else
     echo running on mac os x
+    echo installing fftw with homebrew
+    sudo brew install fftw
     echo fftw in homebrew is not compiled with float support
     echo compiling fftw from source with --enable-float
     wget http://www.fftw.org/fftw-3.3.4.tar.gz
