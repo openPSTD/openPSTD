7ad9335f717610aec80b345fdc84ca95e652a386
Michiel Fortuin <michiel.fortuin@gmail.com>
2016-02-01 13:10:13 +0100

    Added a single test file function in CMake

--- test/test.cmake
+++ test/test.cmake
@@ -1,13 +1,25 @@
 #------------------------------------
 # Test
+
 # Source files of the CLI and GUI(no Kernel and no Shared, they are linked)
 set(SOURCE_FILES_TEST ${SOURCE_FILES_TEST} ${SOURCE_FILES_GUI} ${SOURCE_FILES_CLI})
-# GUI test files
-set(SOURCE_FILES_TEST ${SOURCE_FILES_TEST} test/GUI/Edges-test.cpp)
-# Kernel test files
-set(SOURCE_FILES_TEST ${SOURCE_FILES_TEST} test/Kernel/kernel_functions.cpp
-        test/Kernel/Speaker.cpp test/Kernel/Geometry.cpp test/Kernel/Domain.cpp
-        test/Kernel/wave_numbers.cpp)
+
+if(OPENPSTD_TEST_FILE)
+    message(STATUS "Test file: ${OPENPSTD_TEST_FILE}")
+    # the single test file that must be compiled
+    set(SOURCE_FILES_TEST ${SOURCE_FILES_TEST} ${OPENPSTD_TEST_FILE})
+else()
+    message(STATUS "Test file: ALL")
+
+    # GUI test files
+    set(SOURCE_FILES_TEST ${SOURCE_FILES_TEST} test/GUI/Edges-test.cpp)
+    # Kernel test files
+    set(SOURCE_FILES_TEST ${SOURCE_FILES_TEST} test/Kernel/kernel_functions.cpp
+            test/Kernel/Speaker.cpp test/Kernel/Geometry.cpp test/Kernel/Domain.cpp
+            test/Kernel/wave_numbers.cpp)
+endif()
+
+
 add_executable(OpenPSTD-test test/main.cpp ${SOURCE_FILES_TEST})
 
 # create extra Qt5 code
