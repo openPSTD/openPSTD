0b936d424a60ced43a1c965c8d5301ec42dd3054
Michiel Fortuin <michiel.fortuin@gmail.com>
2016-02-25 12:52:44 +0100

    fixed bug

--- build/install_add_repo.sh
+++ build/install_add_repo.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
 set -e
 
-if [ ${TARGET} = "windows" ]; then
-
-else
+if [ ${TARGET} != "windows" ]; then
     if [ $TRAVIS_OS_NAME = "linux" ]; then
         echo ============================================================================
         sudo add-apt-repository --yes ppa:beineri/opt-qt551-trusty
