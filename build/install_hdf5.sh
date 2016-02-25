c01910e1c57739522bdf12a1a594a44a1a45b5a3
Michiel Fortuin <michiel.fortuin@gmail.com>
2016-02-25 10:54:39 +0100

    added suppress warning to gcc

--- build/install_hdf5.sh
+++ build/install_hdf5.sh
@@ -6,6 +6,6 @@ wget https://www.hdfgroup.org/ftp/HDF5/current/src/hdf5-1.8.16.tar.bz2
 tar -xjf hdf5-1.8.16.tar.bz2
 cd hdf5-1.8.16
 ./configure
-make -w
+make CFLAGS="-w" CXXFLAGS="-w"
 sudo make install
 cd ../
