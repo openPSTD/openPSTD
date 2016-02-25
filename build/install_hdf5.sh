49e0b81b2ca04c6eaf772bc98e103097bcd6182e
Louisvh <Louis.van.harten@gmail.com>
2016-02-25 16:12:23 +0100

    Explicitly compile hdf5 for 64 bit

--- build/install_hdf5.sh
+++ build/install_hdf5.sh
@@ -5,6 +5,7 @@ echo installing HDF5 with source
 wget https://www.hdfgroup.org/ftp/HDF5/current/src/hdf5-1.8.16.tar.bz2
 tar -xjf hdf5-1.8.16.tar.bz2
 cd hdf5-1.8.16
+CFLAGS="-m64"
 ./configure
 make CFLAGS="-w -fPIC" CXXFLAGS="-w -fPIC"
 sudo make install
