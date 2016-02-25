1397a739c839d5dbc10cd8b978676ba5d3d09853
Michiel Fortuin <michiel.fortuin@gmail.com>
2016-02-25 15:55:23 +0100

    removed wini install step and the test for windows

--- build/test.sh
+++ build/test.sh
@@ -1,21 +1,20 @@
 #!/usr/bin/env bash
 set -e
 
-#if [ ${TRAVIS_OS_NAME} = "linux" ]; then
-#    DEPENDENCY_CHECKER=ldd
-#else
-#    DEPENDENCY_CHECKER=otool\ -L
-#fi
+if [ ${TARGET} != "windows" ]; then
+    if [ ${TRAVIS_OS_NAME} = "linux" ]; then
+        DEPENDENCY_CHECKER=ldd
+    else
+        DEPENDENCY_CHECKER=otool\ -L
+    fi
 
-#echo Dependencies for OpenPSTD-gui
-#${DEPENDENCY_CHECKER} OpenPSTD-gui
+    echo Dependencies for OpenPSTD-gui
+    ${DEPENDENCY_CHECKER} OpenPSTD-gui
 
-#echo Dependencies for OpenPSTD-test
-#${DEPENDENCY_CHECKER} OpenPSTD-test
+    echo Dependencies for OpenPSTD-test
+    ${DEPENDENCY_CHECKER} OpenPSTD-test
 
-if [ ${TARGET} = "windows" ]; then
-    wine OpenPSTD-test.exe --log_format=XML --log_sink=results.xml --log_level=all --report_level=no
-else
     ./OpenPSTD-test --log_format=XML --log_sink=results.xml --log_level=all --report_level=no
+    xsltproc build/report/TestReport.xsl results.xml
 fi
-xsltproc build/report/TestReport.xsl results.xml
+
