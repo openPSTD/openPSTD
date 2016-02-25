8dcff2f7182cea83f0a1af7ce161135566573f00
Michiel Fortuin <michiel.fortuin@gmail.com>
2016-02-25 16:53:26 +0100

    Changed sequence Added more debug info in buildlog

--- build/install_add_repo.sh
+++ build/install_add_repo.sh
@@ -4,7 +4,7 @@ set -e
 if [ ${TARGET} = "windows" ]; then
     echo "deb http://pkg.mxe.cc/repos/apt/debian jessie main" | sudo tee /etc/apt/sources.list.d/mxeapt.list
     sudo apt-key adv --keyserver x-hkp://keys.gnupg.net --recv-keys D43A795B73B16ABE9643FE1AFD8FFF16DB45C6AB
-    sudo apt-get update -qq
+    sudo apt-get update
 else
     if [ $TRAVIS_OS_NAME = "linux" ]; then
         echo ============================================================================
