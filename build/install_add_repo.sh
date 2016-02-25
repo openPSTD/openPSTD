85723e17fd0a7f02cb3571a664b59fbbb5b4835d
Michiel Fortuin <michiel.fortuin@gmail.com>
2016-02-25 15:30:00 +0100

    updated add_repo

--- build/install_add_repo.sh
+++ build/install_add_repo.sh
@@ -2,8 +2,8 @@
 set -e
 
 if [ ${TARGET} = "windows" ]; then
-    sudo echo "deb http://pkg.mxe.cc/repos/apt/debian wheezy main" > /etc/apt/sources.list.d/mxeapt.list
-    sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys D43A795B73B16ABE9643FE1AFD8FFF16DB45C6AB
+    echo "deb http://pkg.mxe.cc/repos/apt/debian jessie main" | sudo tee /etc/apt/sources.list.d/mxeapt.list
+    sudo apt-key adv --keyserver x-hkp://keys.gnupg.net --recv-keys D43A795B73B16ABE9643FE1AFD8FFF16DB45C6AB
     sudo apt-get update --qq
 else
     if [ $TRAVIS_OS_NAME = "linux" ]; then
