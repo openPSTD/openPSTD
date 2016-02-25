5912c1cb91435e73386ebe8e6cc0f6722d01b0cf
Louisvh <Louis.van.harten@gmail.com>
2016-02-25 12:41:12 +0100

    Travis mac debugging

--- build/compile.sh
+++ build/compile.sh
@@ -4,16 +4,22 @@ set -e
 if [ ${TRAVIS_OS_NAME} = "linux" ]; then
     QT5DIR=/opt/qt55
     PACKAGE=TGZ
+
+    FFTWFLIB=/usr/lib/x86_64-linux-gnu/libfftw3f.a
+    FFTWFSHAREDOBJ=/usr/lib/x86_64-linux-gnu/libfftw3f.so
 else
     QT5DIR=/usr/local/opt/qt5
     PACKAGE=Bundle
+
+    FFTWFLIB=/usr/local/libfftw/libfftw3f.a
+    FFTWFSHAREDOBJ=/usr/local/libfftw/libfftw3f.so
 fi
 
 cmake \
 	-D EIGEN_INCLUDE:PATH=$PWD/eigen \
 	-D Qt5_DIR:PATH=${QT5DIR} \
-	-D FFTWF_LIBRARY:PATH=/usr/lib/x86_64-linux-gnu/libfftw3f.a \
-	-D FFTWF_SHARED_OBJECT:PATH=/usr/lib/x86_64-linux-gnu/libfftw3f.so \
+	-D FFTWF_LIBRARY:PATH=${FFTWFLIB} \
+	-D FFTWF_SHARED_OBJECT:PATH=${FFTWFSHAREDOBJ} \
 	-D HDF5_INCLUDE:PATH=$PWD/hdf5-1.8.16/hdf5/include \
 	-D HDF5_LIBRARY:PATH=$PWD/hdf5-1.8.16/hdf5/lib/libhdf5.a \
 	-D HDF5_HL_LIBRARY:PATH=$PWD/hdf5-1.8.16/hdf5/lib/libhdf5_hl.a \
