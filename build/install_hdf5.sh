866288d4e83ee365d1786d5560f117fb89edefad
Michiel Fortuin <michiel.fortuin@gmail.com>
2016-02-28 16:13:49 +0100

    Merged the windows compilation

--- build/install_hdf5.sh
+++ build/install_hdf5.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/env bash
 set -e
 
-echo installing HDF5 with source
-wget https://www.hdfgroup.org/ftp/HDF5/current/src/hdf5-1.8.16.tar.bz2
-tar -xjf hdf5-1.8.16.tar.bz2
-cd hdf5-1.8.16
-CFLAGS="-m64"
-./configure
-make CFLAGS="-w -fPIC" CXXFLAGS="-w -fPIC"
-sudo make install
-cd ../
+if [ ${TARGET} = "win64" ]; then
+    sudo apt-get -y install mxe-${WINTARGET}-hdf5
+else
+    echo installing HDF5 with source
+    wget https://www.hdfgroup.org/ftp/HDF5/current/src/hdf5-1.8.16.tar.bz2
+    tar -xjf hdf5-1.8.16.tar.bz2
+    cd hdf5-1.8.16
+    ./configure
+    make CFLAGS="-w -fPIC" CXXFLAGS="-w -fPIC"
+    sudo make install
+    cd ../
+fi
