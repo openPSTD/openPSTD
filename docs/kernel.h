b2de28b8f58a794a2c7e1a51449e4e8cc2794deb
0mar <omsrichardson@gmail.com>
2016-03-02 13:55:19 +0100

    Included the kernel structure page

---
+++ docs/kernel.h
@@ -0,0 +1,19 @@
+/**
+ * \page kernelstructure
+ * # Structure of the kernel
+ * This page describes the interaction between the classes that are responsible for the scientific computation in openPSTD.
+ *
+ * ## Component list
+ * A list with all the module in the kernel can be found in [here](@ref OpenPSTD::Kernel).
+ *
+ * ## Composition structure
+ * From PSTDkernel to Point
+ *
+ * Some UML diagram too
+ *
+ * ## Initialization steps
+ * First..., and then
+ *
+ * ## Running steps
+ * RK6 and stuff
+ */
--- docs/kernel_structure.h
+++
@@ -1,7 +0,0 @@
-/**
- * \page kernelstructure
- * # Structure of the kernel
- * This page describes the interaction between the classes that are responsible for the scientific computation in openPSTD.
- *
- * ## 
- */
