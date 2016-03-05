7fbf3eb4937ca89d7c340e8b1a3813104b7b3b87
Louisvh <Louis.van.harten@gmail.com>
2016-03-05 18:23:26 +0100

    Removed copying artifact

--- build/install_hdf5.sh
+++ build/install_hdf5.sh
@@ -15,12 +15,12 @@ else
         cd ../
     else
         echo installing HDF5 with source
-       wget https://www.hdfgroup.org/ftp/HDF5/current/src/hdf5-1.8.16.tar.bz2
-       tar -xjf hdf5-1.8.16.tar.bz2
-       cd hdf5-1.8.16
-       ./configure
-       make CFLAGS="-w -fPIC" CXXFLAGS="-w -fPIC -stdlib=libc++"
-       sudo make install
-       cd ../  
+       wget https://www.hdfgroup.org/ftp/HDF5/current/src/hdf5-1.8.16.tar.bz2
+       tar -xjf hdf5-1.8.16.tar.bz2
+       cd hdf5-1.8.16
+       ./configure
+       make CFLAGS="-w -fPIC" CXXFLAGS="-w -fPIC -stdlib=libc++"
+       sudo make install
+       cd ../  
     fi
 fi
