1c8c6522c6b6d43b84cf0dd5ac7cb4d0a64bdf96
Louisvh <Louis.van.harten@gmail.com>
2016-03-05 18:15:36 +0100

    Compile hdf with libc++ on mac

--- build/install_hdf5.sh
+++ build/install_hdf5.sh
@@ -4,12 +4,23 @@ set -e
 if [ ${TARGET} = "win64" ]; then
     sudo apt-get -y install mxe-${WINTARGET}-hdf5
 else
-    echo installing HDF5 with source
-    wget https://www.hdfgroup.org/ftp/HDF5/current/src/hdf5-1.8.16.tar.bz2
-    tar -xjf hdf5-1.8.16.tar.bz2
-    cd hdf5-1.8.16
-    ./configure
-    make CFLAGS="-w -fPIC" CXXFLAGS="-w -fPIC"
-    sudo make install
-    cd ../
+    if [ ${TRAVIS_OS_NAME} = "linux" ]; then
+        echo installing HDF5 with source
+        wget https://www.hdfgroup.org/ftp/HDF5/current/src/hdf5-1.8.16.tar.bz2
+        tar -xjf hdf5-1.8.16.tar.bz2
+        cd hdf5-1.8.16
+        ./configure
+        make CFLAGS="-w -fPIC" CXXFLAGS="-w -fPIC"
+        sudo make install
+        cd ../
+    else
+        echo installing HDF5 with source
+       wget https://www.hdfgroup.org/ftp/HDF5/current/src/hdf5-1.8.16.tar.bz2
+       tar -xjf hdf5-1.8.16.tar.bz2
+       cd hdf5-1.8.16
+       ./configure
+       make CFLAGS="-w -fPIC" CXXFLAGS="-w -fPIC -stdlib=libstdc++"
+       sudo make install
+       cd ../  
+    fi
 fi
