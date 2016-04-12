b454c0a598bad1da5626e68aadf5a93e13622364
Michiel Fortuin <michiel.fortuin@gmail.com>
2016-04-12 10:27:07 +0200

    Added debug info in the test script

--- build/test.sh
+++ build/test.sh
@@ -15,6 +15,7 @@ if [ ${TARGET} != "win64" ]; then
     ${DEPENDENCY_CHECKER} OpenPSTD-test
 
     ./OpenPSTD-test --log_format=XML --log_sink=results.xml --log_level=all --report_level=no
+    cat results.xml
     xsltproc build/report/TestReport.xsl results.xml
 fi
 
