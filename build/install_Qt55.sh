3f1e9b912ffcf4bffe9188ea0e5899b11e620fb4
Michiel Fortuin <michiel.fortuin@gmail.com>
2016-04-28 13:56:04 +0200

    Removed qtcreator from the installation

--- build/install_Qt55.sh
+++ build/install_Qt55.sh
@@ -8,7 +8,7 @@ if [ $TRAVIS_OS_NAME = "linux" ]; then
     echo running on linux
     echo installing Qt with apt-get
     sudo apt-get -y install qt553d qt55canvas3d
-    sudo apt-get -y install qtcreator-latest
+    #sudo apt-get -y install qtcreator-latest
     #sudo apt-get -y install qt5-default
 else
     echo running on mac os x
