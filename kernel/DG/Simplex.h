83d94b0963affb0a712ec4be91f731ad2412c93b
Michiel Fortuin <michiel.fortuin@gmail.com>
2016-11-07 12:02:29 +0100

    Implemented Simplex2DP function

---
+++ kernel/DG/Simplex.h
@@ -0,0 +1,67 @@
+//////////////////////////////////////////////////////////////////////////
+// This file is part of openPSTD.                                       //
+//                                                                      //
+// openPSTD is free software: you can redistribute it and/or modify     //
+// it under the terms of the GNU General Public License as published by //
+// the Free Software Foundation, either version 3 of the License, or    //
+// (at your option) any later version.                                  //
+//                                                                      //
+// openPSTD is distributed in the hope that it will be useful,          //
+// but WITHOUT ANY WARRANTY; without even the implied warranty of       //
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the        //
+// GNU General Public License for more details.                         //
+//                                                                      //
+// You should have received a copy of the GNU General Public License    //
+// along with openPSTD.  If not, see <http://www.gnu.org/licenses/>.    //
+//                                                                      //
+//////////////////////////////////////////////////////////////////////////
+
+//////////////////////////////////////////////////////////////////////////
+//
+// Date: 7-11-2016
+//
+//
+// Authors: M. R. Fortuin
+//
+//
+// Purpose:
+//
+//
+//////////////////////////////////////////////////////////////////////////
+
+#ifndef OPENPSTD_SIMPLEX_H
+#define OPENPSTD_SIMPLEX_H
+
+#include <cmath>
+#include <Eigen/Dense>
+#include "GeneralTypes.h"
+#include "Jacobi.h"
+
+
+namespace OpenPSTD
+{
+    namespace Kernel
+    {
+        namespace DG
+        {
+            /**
+             * Evaluate 2D orthonormal polynomial on simplex at (a,b) of order (i,j).
+             * @param a
+             * @param b
+             * @param i
+             * @param j
+             * @return
+             */
+            template <typename SimpleType>
+            VectorX<SimpleType> Simplex2DP(VectorX<SimpleType> a, VectorX<SimpleType> b, int i, int j)
+            {
+                ArrayX<SimpleType> h1 = JacobiP(a, (SimpleType)0, (SimpleType)0, i);
+                ArrayX<SimpleType> h2 = JacobiP(b, (SimpleType)2*i+1, (SimpleType)0, j);
+                ArrayX<SimpleType> tmp = b.unaryExpr([i](SimpleType x){return std::pow(1-x, i); });
+                return 1.4142135623730951*h1*h2*tmp;//1.4142135623730951 == sqrt(2.0)
+
+            }
+        }
+    }
+}
+#endif //OPENPSTD_SIMPLEX_H
--- test/test.cmake
+++ test/test.cmake
@@ -17,6 +17,7 @@ set(SOURCE_FILES_TEST_DG
         test/Kernel/DG/DMatrix.cpp
         test/Kernel/DG/GeometricFactors.cpp
         test/Kernel/DG/Lift.cpp
+        test/Kernel/DG/Simplex.cpp
         test/Kernel/DG/Nodes.cpp)
 
 #set(SOURCE_FILES_TEST_DG test/Kernel/DG/WarpFactor.cpp)
