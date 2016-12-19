0aa5bd50b17797dc4ee556b2f39c4245da4effa7
Louisvh <Louisvanharten@gmail.com>
2016-12-19 11:55:37 +0100

    added openmp compilation settings

--- dependencies.cmake
+++ dependencies.cmake
@@ -40,6 +40,14 @@ if (NOT Boost_FOUND)
 endif ()
 
 #------------------------------------
+# OpenMP
+find_package(OpenMP)
+if (OPENMP_FOUND)
+    set (CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${OpenMP_C_FLAGS}")
+    set (CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${OpenMP_CXX_FLAGS}")
+endif()
+
+#------------------------------------
 # QT5
 set(CMAKE_INCLUDE_CURRENT_DIR ON)
 
