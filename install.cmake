dcd507a5457f4df73e0e1e29e49cf799f6822387
Michiel <michiel.fortuin@gmail.com>
2016-12-19 15:43:26 +0100

    Added winpthread to the install

--- install.cmake
+++ install.cmake
@@ -48,6 +48,7 @@ elseif(OPENPSTD_SYSTEM_NAME STREQUAL "win64")
     WinInstallLib(libbz2)
     WinInstallLib(libiconv-2)
     WinInstallLib(libgomp-1)
+    WinInstallLib(winpthread-1)
 
     #extra qt5 plugin for windows
     install(FILES ${Boost_LIBRARY_DIRS}/../qt5/plugins/platforms/qwindows.dll DESTINATION platforms)
