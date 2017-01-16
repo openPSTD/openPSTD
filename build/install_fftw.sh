561b6d88cd8f8b8c452cb03f4f06450921a090d3
Louisvh <Louisvanharten@gmail.com>
2017-01-16 13:21:22 +0100

    added missing fftw flag

--- build/install_fftw.sh
+++ build/install_fftw.sh
@@ -12,7 +12,7 @@ else
         wget http://www.fftw.org/fftw-3.3.5.tar.gz
         tar -xzf fftw-3.3.5.tar.gz
         cd fftw-3.3.5
-        ./configure --enable-float --enable-sse --enable-sse2 --enable-avx --enable-avx2 --enable-generic-simd128 CFLAGS="-fPIC" CPPFLAGS="-fPIC" > /dev/null
+        ./configure --enable-shared --enable-float --enable-sse --enable-sse2 --enable-avx --enable-avx2 --enable-generic-simd128 CFLAGS="-fPIC" CPPFLAGS="-fPIC" > /dev/null
         make > /dev/null
         sudo make install
     else
@@ -22,7 +22,7 @@ else
         wget http://www.fftw.org/fftw-3.3.5.tar.gz
         tar -xzf fftw-3.3.5.tar.gz
         cd fftw-3.3.5
-        ./configure --enable-float --enable-sse --enable-sse2 --enable-avx --enable-avx2 --enable-generic-simd128 CXX="g++ -stdlib=libstdc++ -arch x86_64 -fPIC" > /dev/null
+        ./configure --enable-shared --enable-float --enable-sse --enable-sse2 --enable-avx --enable-avx2 --enable-generic-simd128 CXX="g++ -stdlib=libstdc++ -arch x86_64 -fPIC" > /dev/null
         make > /dev/null
         sudo make install
     fi
