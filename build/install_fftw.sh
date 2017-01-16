7086699072f3ca8614c4fe6083c7bf5d344a49e6
Louisvh <Louisvanharten@gmail.com>
2017-01-16 11:28:28 +0100

    added thread safe fftw version to travis

--- build/install_fftw.sh
+++ build/install_fftw.sh
@@ -6,17 +6,23 @@ if [ ${TARGET} = "win64" ]; then
 else
     if [ ${TRAVIS_OS_NAME} = "linux" ]; then
         echo running on linux
-        echo installing fftw
-        sudo apt-get update
-        sudo apt-get  --yes --force-yes install libfftw3-dev
+        echo fftw in repo is compiled with --enable-debug,
+        echo which breaks thread safety.
+        echo compiling fftw from source with --enable-float
+        wget http://www.fftw.org/fftw-3.3.5.tar.gz
+        tar -xzf fftw-3.3.5.tar.gz
+        cd fftw-3.3.5
+        ./configure --enable-float --enable-sse --enable-sse2 --enable-avx --enable-avx2 --enable-avx512 --enable-generic-simd128 CXX="g++ -stdlib=libstdc++ -arch i386 -arch x86_64"
+        make
+        sudo make install
     else
         echo running on mac os x
         echo fftw in homebrew is not compiled with float support
         echo compiling fftw from source with --enable-float
-        wget http://www.fftw.org/fftw-3.3.4.tar.gz
-        tar -xzf fftw-3.3.4.tar.gz
-        cd fftw-3.3.4
-        ./configure --enable-float --enable-sse CXX="g++ -stdlib=libstdc++ -arch i386 -arch x86_64"
+        wget http://www.fftw.org/fftw-3.3.5.tar.gz
+        tar -xzf fftw-3.3.5.tar.gz
+        cd fftw-3.3.5
+        ./configure --enable-float --enable-sse --enable-sse2 --enable-avx --enable-avx2 --enable-avx512 --enable-generic-simd128 CXX="g++ -stdlib=libstdc++ -arch i386 -arch x86_64"
         make
         sudo make install
     fi
