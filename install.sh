2ae88bcac105daab07c008f609168e5fca8fd473
JeroenDonners <jeroen.donners@gmail.com>
2017-07-17 23:04:33 +0200

    Attempt to fix initial travis.yml.

--- install.sh
+++ install.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-sudo apt-get -y install qt5-default
+sudo add-apt-repository --yes ppa:ubuntu-sdk-team/ppa
+sudo apt-get update -qq
+sudo apt-get install qtbase5-dev
+sudo apt-get install qt5-default
