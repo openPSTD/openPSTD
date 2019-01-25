e30f05a64213736a75f5c39afc25dad9dc2fdbfb
JeroenDonners <j.a.e.donners@student.tue.nl>
2019-01-25 16:32:06 +0100

    Created branch for new Backend version.

--- build/install_win.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-if [ ${TARGET} = "win64" ]; then
-    echo install wine
-    sudo apt-get install wine
-fi
--- unqlite/unqlite.cmake
+++
@@ -1,7 +0,0 @@
-# Unqlite library
-add_library(unqlite SHARED unqlite/unqlite.c)
-
-# make sure the functions are hidden by default
-set_target_properties(unqlite PROPERTIES
-        CXX_VISIBILITY_PRESET hidden
-        VISIBILITY_INLINES_HIDDEN 1)
